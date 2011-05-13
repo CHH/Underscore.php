e2094dbcdfaf0a12a48fec6d88cd51f58b4eed6f
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-05-13 12:27:47 +0200

    Added test for memoize()

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -2,9 +2,19 @@
 
 namespace Underscore\Test;
 
+use Underscore as _;
+
 class FunctionTest extends \PHPUnit_Framework_TestCase
 {
-    function test()
+    function testMemoize()
     {
+        function fib($n) {
+            return $n < 2 ? $n : fib($n - 1) + fib($n - 2);
+        }
+
+        $fastFib = _\memoize("\\Underscore\\Test\\fib");
+
+        $this->assertEquals(55, fib(10), "a memoized version of fibonacci produces identical results");
+        $this->assertEquals(55, $fastFib(10), "a memoized version of fibonacci produces identical results");
     }
 }
